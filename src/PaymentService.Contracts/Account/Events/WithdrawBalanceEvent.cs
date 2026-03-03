e1378f0c3e6afae3fa3dbd3d24f25a94d645feba
Raufjon98 <raufjonaliboev77@gmail.com>
2026-03-03 09:42:43 +0500

    Added withdrawBalanceEvent for order pay

--- src/PaymentService.Contracts/Account/Events/WithdrawBalanceEvent.cs
+++ src/PaymentService.Contracts/Account/Events/WithdrawBalanceEvent.cs
@@ -1,8 +1,11 @@
+using PaymentService.Contracts.Transaction.Enum;
+
 namespace PaymentService.Contracts.Account.Events;
 
-public class WithDrawalEvent
+public class WithdrawBalanceEvent
 {
     public string SourceId { get; set; }
-    public Guid AccountId { get; set; }
+    public Guid CustomerId { get; set; }
     public decimal Amount { get; set; }
+    public TransationStatus TransactionStatus { get; set; }
 }
